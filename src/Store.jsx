0f5019b37ff14165c53de8255b26847bc53daae0
Sang Min Shin <sang.shin@deutschinc.com>
2020-09-30 23:24:30 -0400

    changed the files for AWS S3 static site hosting

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,30 +1,30 @@
 import React, { Component, createContext } from 'react';
-import data from './data/data.json'; //DEVELOPMENT MODE
-// import axios from 'axios';
+// import data from './data/data.json'; //DEVELOPMENT MODE
+import axios from 'axios';
 
 const Context = createContext();
 const { Provider } = Context
 // const dataUrl = 'http://localhost:3000/api'; //DEVELOPMENT MODE
-// const dataUrl = '/api'; // DEPLOY MODE
+const dataUrl = '/api'; // DEPLOY MODE
 
 
 class Store extends Component{
   state = {
-    projects: data //DEVELOPMENT MODE
-    // projects: [] // DEPLOY MODE
+    // projects: data //DEVELOPMENT MODE
+    projects: [] // DEPLOY MODE
   }
 
-  // componentDidMount(){
-  //   axios.get(dataUrl, { 
-  //     headers: { 
-  //       'crossDomain': true, 
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json',
-  //       'mode': 'cors'
-  //       }
-  //     })
-  //     .then(res => this.setState({ projects: res.data }))
-  // }
+  componentDidMount(){
+    axios.get(dataUrl, { 
+      headers: { 
+        'crossDomain': true, 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'mode': 'cors'
+        }
+      })
+      .then(res => this.setState({ projects: res.data }))
+  }
 
   render(){
     return (
